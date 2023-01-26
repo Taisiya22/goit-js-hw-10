db94f70c937f3987b5108cff5721c45c5a1299ea
Taisiya Tomashevska <taya22011985@gmail.com>
2023-01-26 20:53:20 +0200

    fix

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+const ENDPOINTS = 'https://restcountries.com/v3.1/name/${name}'
+const searchParams = new URLSearchParams(
+    {
+
+    }
+)
+
+export function fetchCountries(name) {
+   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(responce => {
+            if (!responce.ok) {
+                throw new Error(responce.status);
+            }
+            return responce.json();
+        });
+}
+        
--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+
+const inputText = document.getElementById('search-box');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
+
+inputText.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
+
+
+function onSearchCountry(e) { 
+  let inputValue = e.target.value.trim();
+  if (inputValue === '') {
+    list.innerHTML = '';
+    info.innerHTML = '';
+    return;
+   } 
+  
+    fetchCountries(inputValue)
+      .then(renderCountryList)
+      .catch(onError)
+ 
+}
+
+function renderCountryList(countries) { 
+   list.innerHTML = countries.reduce((acc, { name: { official }, flags: { svg } }) => {return acc +  `<li>
+  <img src="${svg}" alt="flag" width = 200px>
+  <p><b>${official}</b></p>
+</li>`}, '')
+  
+  if (countries.length === 1) { 
+     info.innerHTML = countries.reduce((acc, { flags: { svg }, name: { official }, capital, population, languages }) => {
+      return acc +  
+      `<p><b>Capital:</b> ${capital}</p>
+       <p><b>Population:</b> ${population}</p>
+       <p><b>Languages:</b> ${Object.values(languages)}</p>`
+       }, '')
+  }
+
+  if (countries.length > 10) {
+    info.innerHTML = '';
+    list.innerHTML = '';
+    Notiflix.Notify.info('"Too many matches found. Please enter a more specific name."');
+  }
+  
+}
+
+function onError(error) { 
+  console.error(error);
+  Notiflix.Notify.failure("Oops, there is no country with that name");
+   list.innerHTML = '';
+    info.innerHTML = '';
+  return error;
+}
